50de983e0deb69ff1f66612a8bb561b59ffec81d
Akhil <bishtakhil04@gmail.com>
2026-02-14 04:15:51 +0530

    CORS

--- src/main/java/com/akhil/insightforge/config/CorsConfig.java
+++ src/main/java/com/akhil/insightforge/config/CorsConfig.java
@@ -14,7 +14,8 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5173")
+                        .allowedOrigins("http://localhost:5173",
+                                "https://insightforge-ui.vercel.app/")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*");
             }
